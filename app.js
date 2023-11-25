52a3ac7a36c1d94258dc46a22785c3d77d4207fc
Anugo1 <ekehumphery@gmail.com>
2023-11-25 04:55:04 +0100

    updated my env

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-
+const connectdb = require('./db/connect')
+const { AsyncLocalStorage } = require('async_hooks')
 
 //middleware
 
@@ -16,4 +18,13 @@ app.use('/api/v1/tasks',tasks)
 
 const port = 8000
 
-app.listen(port, console.log(`server is listening @ port ${port}...`))
+const start = async () => {
+    try {
+        await connectdb()
+        app.listen(port, console.log(`server is listening @ port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,12 @@
-const connection_string = ('mongodb+srv://ekehumphery:anugo1@cluster0.7forwyr.mongodb.net/?retryWrites=true&w=majority') 
+const { error } = require('console')
+const { url } = require('inspector')
+const mongoose = require('mongoose')
+
+const connection_string =
+'mongodb+srv://ekehumphery:anugo1@cluster0.7forwyr.mongodb.net/task-manager?retryWrites=true&w=majority'
+
+const connectdb = (url) =>{
+return mongoose.connect(connection_string)
+}
+
+module.exports = connectdb
